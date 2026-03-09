0271d4d5f3ae0f9671642650678eb2cb3a942b06
Murphy <2822887579@qq.com>
2026-03-09 19:59:02 +0800

    refactor: fully remove old NotesView and Sidebar, integrate to immersive feed view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ImmersiveLayout from './components/ImmersiveLayout';
 import SourcesView from './views/SourcesView';
 import FeedView from './views/FeedView';
-import NotesView from './views/NotesView';
 import { AppProvider } from './contexts/AppContext';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
             <Route index element={<Navigate to="/feed" replace />} />
             <Route path="sources" element={<SourcesView />} />
             <Route path="feed" element={<FeedView />} />
-            <Route path="notes" element={<NotesView />} />
           </Route>
         </Routes>
       </BrowserRouter>
